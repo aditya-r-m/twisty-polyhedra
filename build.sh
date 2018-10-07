c61255cabf524b4cf9bea6c898bdc8c7b39b394e
Aditya Mishra <adityam.rtm@gmail.com>
2018-10-07 15:00:41 +0530

    add build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+cat src/classes/Vector.js \
+    src/classes/Quaternion.js \
+    src/classes/Point.js \
+    src/classes/Sticker.js \
+    src/classes/Face.js \
+    src/classes/Cycle.js \
+    src/classes/Puzzle.js \
+    src/classes/Puzzles/Cube.js \
+    src/classes/Puzzles/Tetrahedron.js \
+    src/classes/Puzzles/Octahedron.js \
+    src/menu.js \
+    src/game.js \
+> dist/temp.js
+
+google-closure-compiler-js dist/temp.js > dist/build.js
+
+rm dist/temp.js

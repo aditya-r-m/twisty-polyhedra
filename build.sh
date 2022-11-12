00eeeb43078f17fb93b068273dd33bb32992c9a0
Aditya Mishra <adityam.rtm@gmail.com>
2022-11-12 14:41:39 +0530

    pull from master

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 cat src/utils.js src/classes/Vector.js src/classes/Quaternion.js src/classes/Point.js src/classes/Sticker.js src/classes/Face.js src/classes/Cycle.js src/classes/Puzzle.js src/classes/Puzzles/Cube.js src/classes/Puzzles/Tetrahedron.js src/classes/Puzzles/Octahedron.js src/classes/Puzzles/Dodecahedron.js src/classes/Puzzles/Icosahedron.js src/menu.js src/game.js src/solver.js > dist/temp.js && npx google-closure-compiler dist/temp.js > dist/build.js && rm dist/temp.js
+
